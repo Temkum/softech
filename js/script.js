c2f53b7d5c71ebb0d1aec71c4a6956f8294eb614
Kum Jude Tem <judekum14@gmail.com>
2021-08-13 00:30:01 +0100

    used sass for styling

--- js/script.js
+++ js/script.js
@@ -12,14 +12,13 @@ let themeDots = document.getElementsByClassName("theme-dot");
 for (let i = 0; themeDots.length > i; i++) {
   themeDots[i].addEventListener("click", function () {
     let mode = this.dataset.mode;
-    console.log("Option clicked", mode);
     setTheme(mode);
   });
 }
 
 function setTheme(mode) {
   if (mode == "light") {
-    document.getElementById("theme-style").href = "css/main.css";
+    document.getElementById("theme-style").href = "css/theme.css";
   }
   if (mode == "blue") {
     document.getElementById("theme-style").href = "css/blue.css";
